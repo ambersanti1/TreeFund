dbef404c084e674eebeff007ef2749d421ecc08b
[Jerry Chow] <142858979+JerryChowMX@users.noreply.github.com>
2024-03-03 15:36:15 -0600

    changes to home page

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -7,6 +7,9 @@ const Hero = () => {
       <h1>
         Planting Today, for a Greener Tomorrow.
       </h1>
+      <p> 
+        With your donation today, we'll make sure biodiversity in Mexico keeps on thriving!
+      </p>
       <div className="image-container" >
         <img src={heroImage} alt="People Planting Trees for a better future" />
       </div>
