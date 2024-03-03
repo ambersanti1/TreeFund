cc205548eaabe0bb41233200a3878c9014d20f53
[Jerry Chow] <142858979+JerryChowMX@users.noreply.github.com>
2024-03-03 14:14:02 -0600

    changes

--- client/src/components/Accordion.jsx
+++ client/src/components/Accordion.jsx
@@ -13,27 +13,17 @@ import farTrees from '../Images/Far_trees.webp'
 import React, { useState, useEffect } from 'react';
 
 const Accordion = () => {
-  // State to hold the window width
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
   useEffect(() => {
-    // Handler to call on window resize
     function handleResize() {
-      // Set window width to the state
       setWindowWidth(window.innerWidth);
     }
-
     // Add event listener
     window.addEventListener('resize', handleResize);
-
-    // Call handler right away so state gets updated with initial window size
     handleResize();
-
-    // Remove event listener on cleanup
     return () => window.removeEventListener('resize', handleResize);
-  }, []); // Empty array ensures that effect is only run on mount and unmount
-
-  // Determine the number of items to render based on window width
+  }, []); 
   const itemsToRender = windowWidth < 600 ? 2 : 10;
 
   // Images array to dynamically create list items
