7bf602aff7a5531348dd56c08bce9e3b8edcf4a3
ambersanti <carolinasandoval879@gmail.com>
2024-03-05 20:15:12 -0600

    created connection to mongo

--- server/index.js
+++ server/index.js
@@ -5,18 +5,25 @@ const mongoose = require("mongoose");
 const stripe = require("./routes/stripe");
 const products = require("./products");
 const userRouter = require("./routes/user");
-const cookieParser = require('cookie-parser')
+const cookieParser = require("cookie-parser");
+const path = require("path");
 const app = express();
 
+
 app.use(express.json());
-app.use(cors({
-  origin: ['http://localhost:3000'],
-  credentials: true,
-}));
-app.use(cookieParser())
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+  })
+);
+app.use(express.static(path.join(__dirname + "/public")));
+app.use(cookieParser());
 app.use("/auth", userRouter);
 
-mongoose.connect("mongodb://localhost:27017/authentication");
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/authentication"
+);
 
 app.use("/api/stripe", stripe);
 
