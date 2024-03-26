449aaf99d987af5d3882107482e7ef7fcb0402c4
ambersanti <carolinasandoval879@gmail.com>
2024-03-26 16:52:05 -0600

    added build folder in server

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ const corsOptions = {
   ],
 };
 app.use(cors(corsOptions));
-app.use(express.static(path.join(__dirname + "../client/build")));
+app.use(express.static(path.join(__dirname + "/build")));
 app.use(cookieParser());
 app.use("/auth", userRouter);
 
