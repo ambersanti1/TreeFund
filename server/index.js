eaa1945cdb19b2d4151a77d53a4c6eafd0289833
ambersanti <carolinasandoval879@gmail.com>
2024-03-26 16:36:46 -0600

    Added static client/build in server

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ const corsOptions = {
   ],
 };
 app.use(cors(corsOptions));
-app.use(express.static(path.join(__dirname + "/public")));
+app.use(express.static(path.join(__dirname, "client", "build")));
 app.use(cookieParser());
 app.use("/auth", userRouter);
 
