44f99059fb270510870493c51edb1563b458e651
ambersanti <carolinasandoval879@gmail.com>
2024-03-28 18:25:11 -0600

    Fourth commit to Heroku

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,27 +3,29 @@ import '../styles/Footer.css'
 
 export const Footer = () => {
   return (
-    <div className = "footer-container">
-      <div className = "footer-title">
-        <h1>TreeFund</h1>
-      </div>
+    <div className="footer-container">
+      <div className="footer-cols">
+        <div className="footer-title">
+          <h1>TreeFund</h1>
+        </div>
 
-      <div className= "footer-visit">
-        <h3>Visit</h3>
-        <p>IOS Office, No. 345</p>
-        <p>Paseo Andares, #456</p>
-        <p>Zapopan, Jalisco</p>
-        <p>Mexico</p>
-      </div>
+        <div className="footer-visit">
+          <h3>Visit</h3>
+          <p>IOS Office, No. 345</p>
+          <p>Paseo Andares, #456</p>
+          <p>Zapopan, Jalisco</p>
+          <p>Mexico</p>
+        </div>
 
-      <div className="footer-follow">
-        <h3>Follow</h3>
-        <p>TreeFundX</p>
-        <p>TreeFundFB</p>
-        <p>TreeFund@gmail.com</p>
+        <div className="footer-follow">
+          <h3>Follow</h3>
+          <p>TreeFundX</p>
+          <p>TreeFundFB</p>
+          <p>TreeFund@gmail.com</p>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Footer
